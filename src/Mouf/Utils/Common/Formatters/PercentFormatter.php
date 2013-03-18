0dbad6b349e2b6124f1d6d86271edf9bd6503f41
unknown <hugoaverty@gmail.com>
2013-03-18 17:22:13 +0100

    Adding new percentformatter class

---
+++ src/Mouf/Utils/Common/Formatters/PercentFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mouf\Utils\Common\Formatters;
+
+/**
+ * The PercentFormatter class transform a float into a purcent or 
+ * a percent into a float.
+ * 
+ * @Component
+ */
+class PercentFormatter implements BijectiveFormatterInterface {
+
+    
+    /**
+     * Transform a float into a percentage
+     * @see BijectiveFormatterInterface::format()
+     */
+    public function format($value) {
+        if($value=="") {
+        	return null;
+        }
+        return ($value * 100);
+    }
+    
+    /**
+     * Transform a percentage into a float
+     * @see BijectiveFormatterInterface::unformat()
+     */
+    public function unformat($value) {
+        if($value=="") {
+        	return null;
+        }
+        return ($value/100);
+    }
+    
+}
+
+?>
