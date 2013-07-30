7868c1c7f378b22172f04aeb4517e7e2f240f520
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-07-30 17:43:12 +0200

    add UrlToLinkFormatter

---
+++ src/Mouf/Utils/Common/Formatters/UrlToLinkFormatter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mouf\Utils\Common\Formatters;
+
+/**
+ * 
+ * The UrlToLink class transform any url to a html link <a>
+ * 
+ * @Component
+ * @author Pierre
+ * 
+ */
+class UrlToLinkFormatter implements FormatterInterface {
+
+    /**
+     * Transform any url to a html link <a>
+     * @see FormatterInterface::format()
+     */
+    public function format($value) {
+		$text = preg_replace('/http(s)*:\/\/[0-9a-z\.\:\/-_]+/', '<a href="${0}" target="_blank">${0}</a>', $value);
+		return $text;
+    }
+    
+}
+?>
